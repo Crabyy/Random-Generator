532fd2c6ad1a6564f5dddb962191929544e8b18d
StuckCrabs <90059694+StuckCrabs@users.noreply.github.com>
2023-06-07 22:08:25 +0800

    committed

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>CRUD Example</title>
+    <title>Personal Random Generator</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
